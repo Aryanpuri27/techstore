a281f8a5f5b1bd920e81af342e9a7ba22553637e
Aryanpuri27 <puriaryan422@gmail.com>
2024-12-17 00:34:56 +0530

    return to aivora

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "ReddixToys",
-  description: "ReddixToys a place to buy 3d printed products",
+  title: "Aivora",
+  description: "Aivora a place to buy 3d printed products",
 };
 
 export default async function RootLayout({
